b477e763a9b8cb1aa45904ae7cc287e744cc7525
sublum <sublum@126.com>
2018-10-14 20:15:04 +0800

    修改

--- hello_git/src/com/itheima/git/FirstGit.java
+++ hello_git/src/com/itheima/git/FirstGit.java
@@ -10,4 +10,10 @@ public class FirstGit {
         System.out.println("hello,git");
         System.out.println("hello,git");
     }
+
+    public void aa(){
+        int i=10;
+        i+=50;
+        System.out.println(i);
+    }
 }
